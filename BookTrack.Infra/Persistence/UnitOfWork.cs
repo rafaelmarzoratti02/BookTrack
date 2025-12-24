eaf4d58adb6025546f618de6e56d168417e20b81
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2025-12-23 22:53:53 -0300

    Adicionando o  dispatcher de events ao salvar mudanças no banco

--- BookTrack.Infra/Persistence/UnitOfWork.cs
+++ BookTrack.Infra/Persistence/UnitOfWork.cs
@@ -1,26 +1,53 @@
-﻿using BookTrack.Core.Repositories;
+﻿using BookTrack.Core.Entitites;
+using BookTrack.Core.Events;
+using BookTrack.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookTrack.Infra.Persistence;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly BookTrackDbContext _dbContext;
+    private readonly IDomainEventDispatcher _eventDispatcher;
+
     public UnitOfWork(
-        BookTrackDbContext dbContext, IBookRepository books, IReviewRepository reviews, IUserRepository users)
+        BookTrackDbContext dbContext,
+        IBookRepository books,
+        IReviewRepository reviews,
+        IUserRepository users,
+        IDomainEventDispatcher eventDispatcher)
     {
         _dbContext = dbContext;
         Books = books;
         Reviews = reviews;
         Users = users;
+        _eventDispatcher = eventDispatcher;
     }
 
     public IBookRepository Books { get; }
     public IReviewRepository Reviews { get; }
     public IUserRepository Users { get; }
-    
+
     public async Task<int> CompleteAsync()
     {
-       return await _dbContext.SaveChangesAsync();
+        var entitiesWithEvents = _dbContext.ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity)
+            .ToList();
+
+        var domainEvents = entitiesWithEvents
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+        
+        foreach (var entity in entitiesWithEvents)
+        {
+            entity.ClearDomainEvents();
+        }
+        
+        await _eventDispatcher.DispatchAsync(domainEvents);
+        
+        return await _dbContext.SaveChangesAsync();
     }
 
     public void Dispose()
