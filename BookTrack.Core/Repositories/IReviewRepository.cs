ae190a2b32ccba9c455021785939851ccb8eae2a
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2025-12-23 22:50:21 -0300

    removendo metodo de atualizar media do livro do repository

--- BookTrack.Core/Repositories/IReviewRepository.cs
+++ BookTrack.Core/Repositories/IReviewRepository.cs
@@ -7,6 +7,5 @@ public interface IReviewRepository
     Task<List<Review>> GetAllByBookId(int bookId);
     Task<Review> GetById(int id);
     Task<int> Add(Review review);
-    Task UpdateBookAverageRating(int bookId);
     Task<bool> ReviewAlreadyExists(int bookId,int userId);
 }
